e7b426b9bddea0f075dc241d81f54e5d90ec70ae
Leila Mansouri <l71mansouri@gmail.com>
2025-01-23 21:37:49 +0330

    implement examples class

--- src/com/github/LeilaM99/Examples/Examples.java
+++ src/com/github/LeilaM99/Examples/Examples.java
@@ -1,4 +1,46 @@
 package com.github.LeilaM99.Examples;
 
 public class Examples {
+
+        public static void main(String[] args) {
+
+            Movie myMovie = new Movie();
+            myMovie.title = "Inception";
+            myMovie.director = "C. Nolan";
+            myMovie.play();
+
+            Laptop myLaptop = new Laptop();
+            myLaptop.brand = "ASUS";
+            myLaptop.model = "FX502VM";
+            myLaptop.powerOn();
+        }
+
+
+
+
+//    Ex1
+    static class Movie {
+        String title;
+        String director;
+
+        void play() {
+            System.out.println("Playing the movie: " + title);
+        }
+    }
+//  Ex2
+    static class Laptop {
+        String brand;
+        String model;
+
+        void powerOn() {
+            System.out.println(brand + " " + model + " is powering on.");
+        }
+
+
+    }
+
+
+
+
 }
+
